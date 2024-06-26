16dbce7893966aba6c2befb9a5cc2f848b7d19e2
VinAiros <vinicius.a.assuncao2003@gmail.com>
2024-06-26 03:17:46 -0300

    feat: Implement lazy loading for images in CrawlerService

--- src/services/CrawlerService.ts
+++ src/services/CrawlerService.ts
@@ -1,48 +1,45 @@
-import puppeteer, { Browser, Page } from 'puppeteer';
+import puppeteer, { Browser, Page, Target } from 'puppeteer';
 import PuppeteerConfig from '../config/PuppeteerConfig';
 import Logger from '../utils/Logger';
 import { ProcessDetails } from '../interfaces/ProcessDetails';
 
 class CrawlerService {
   private browser!: Browser;
-  private page!: Page;
-
-  constructor() {
-    this.init();
-  }
 
   private async init() {
     this.browser = await puppeteer.launch(PuppeteerConfig);
-    this.page = await this.browser.newPage();
-    Logger.info('Navegador iniciado e nova aba aberta');
+    Logger.info('Navegador iniciado');
   }
 
   public async fetchProcessDetails(
     processNumber: string,
   ): Promise<ProcessDetails | null> {
     try {
-      await this.page.goto(
+      await this.init();
+      const page: Page = await this.browser.newPage();
+      Logger.info('Nova aba aberta');
+      await page.goto(
         'https://pje-consulta-publica.tjmg.jus.br/pje/ConsultaPublica/listView.seam',
       );
       Logger.info('Página carregada');
 
-      await this.page.waitForSelector(
+      await page.waitForSelector(
         '#fPP\\:numProcesso-inputNumeroProcessoDecoration\\:numProcesso-inputNumeroProcesso',
       );
-      await this.page.type(
+      await page.type(
         '#fPP\\:numProcesso-inputNumeroProcessoDecoration\\:numProcesso-inputNumeroProcesso',
         processNumber,
       );
       Logger.info('Número do processo inserido');
 
-      await this.page.waitForSelector('#fPP\\:searchProcessos');
-      await this.page.click('#fPP\\:searchProcessos');
+      await page.waitForSelector('#fPP\\:searchProcessos');
+      await page.click('#fPP\\:searchProcessos');
       Logger.info('Botão "Pesquisar" clicado');
 
-      await this.page.waitForSelector('td.rich-table-cell');
+      await page.waitForSelector('td.rich-table-cell');
       Logger.info('Tabela de resultados carregada');
 
-      await this.page.evaluate(() => {
+      await page.evaluate(() => {
         const link = document.querySelector(
           'td.rich-table-cell a[title="Ver Detalhes"]',
         );
@@ -50,10 +47,20 @@ class CrawlerService {
       });
       Logger.info('Link "Ver Detalhes" clicado');
 
-      await this.page.waitForSelector('body');
+      const newPagePromise: Promise<Page> = new Promise((resolve, reject) => {
+        this.browser.once('targetcreated', async (target: Target) => {
+          const newPage = await target.page();
+          newPage
+            ? resolve(newPage)
+            : reject(new Error('Nova página não encontrada'));
+        });
+      });
+
+      const newPage: Page = await newPagePromise;
+      await newPage.waitForSelector('body');
       Logger.info('Nova aba carregada');
 
-      const processDetails: ProcessDetails = await this.page.evaluate(() => {
+      const processDetails: ProcessDetails = await newPage.evaluate(() => {
         const getFieldText = (selector: string): string | null => {
           const element = document.querySelector(selector);
           return element ? element.textContent?.trim() || null : null;
@@ -82,8 +89,8 @@ class CrawlerService {
         };
       });
 
-      const extractMovements = async (): Promise<string[]> => {
-        const movements = await this.page.evaluate(() => {
+      const extractMovements = async (page: Page): Promise<string[]> => {
+        const movements = await page.evaluate(() => {
           const rows = document.querySelectorAll(
             'table#j_id134\\:processoEvento tbody tr',
           );
@@ -99,7 +106,25 @@ class CrawlerService {
         return movements;
       };
 
-      const totalPages = await this.page.evaluate(() => {
+      const delay = (ms: number) =>
+        new Promise((resolve) => setTimeout(resolve, ms));
+
+      const changeMovementPage = async (page: Page, pageIndex: number) => {
+        await page.evaluate((index) => {
+          const input = document.querySelector<HTMLInputElement>(
+            'input#j_id134\\:j_id531\\:j_id532Input',
+          );
+          if (input) {
+            input.value = index.toString();
+            input.dispatchEvent(new Event('change', { bubbles: true }));
+          }
+        }, pageIndex);
+        await page.waitForSelector('span#j_id134\\:processoEventoMessages', {
+          hidden: true,
+        });
+      };
+
+      const totalPages = await newPage.evaluate(() => {
         const rightNumElement = document.querySelector(
           'td.rich-inslider-right-num',
         );
@@ -110,24 +135,11 @@ class CrawlerService {
 
       const allMovements: string[] = [];
       for (let i = 1; i <= totalPages; i++) {
-        const movements = await extractMovements();
+        const movements = await extractMovements(newPage);
         allMovements.push(...movements);
         if (i < totalPages) {
-          await this.page.evaluate((index) => {
-            const input = document.querySelector<HTMLInputElement>(
-              'input#j_id134\\:j_id531\\:j_id532Input',
-            );
-            if (input) {
-              input.value = index.toString();
-              input.dispatchEvent(new Event('change', { bubbles: true }));
-            }
-          }, i + 1);
-          await this.page.waitForSelector(
-            'span#j_id134\\:processoEventoMessages',
-            {
-              hidden: true,
-            },
-          );
+          await delay(2000);
+          await changeMovementPage(newPage, i + 1);
         }
       }
 
@@ -141,6 +153,11 @@ class CrawlerService {
     } catch (error) {
       Logger.error((error as Error).message);
       return null;
+    } finally {
+      if (this.browser) {
+        await this.browser.close();
+        Logger.info('Navegador fechado');
+      }
     }
   }
 }
