68734b1ebebb5f848fa645728330996222003eb7
VinAiros <vinicius.a.assuncao2003@gmail.com>
2024-06-26 02:59:55 -0300

    chore: Refactor routes to use separate file

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,12 @@ import 'express-async-errors';
 import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
-import CrawlerController from './controllers/CrawlerController';
 import Logger from './utils/Logger';
 import { CustomError } from './interfaces/CustomError';
-import ApiController from './controllers/ApiController';
+import routes from './routes/routes';
 
 const app = express();
 const port = 3000;
-
-const crawlerController = new CrawlerController();
-const apiController = new ApiController();
 
 app.use(express.json());
 app.use(cors());
@@ -23,15 +19,7 @@ app.use(
   }),
 );
 
-app.get('/process/:processNumber', (req: Request, res: Response) =>
-  crawlerController.getProcessDetails(req, res),
-);
-app.post('/generate-excel', (req: Request, res: Response) =>
-  apiController.generateExcel(req, res),
-);
-app.post('/paginate-movements', (req: Request, res: Response) =>
-  apiController.paginateMovements(req, res),
-);
+app.use('/', routes);
 
 app.use(
   (error: CustomError, req: Request, res: Response, _next: NextFunction) => {
